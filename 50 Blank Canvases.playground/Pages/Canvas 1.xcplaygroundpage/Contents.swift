1d2aa7bc1da39e45a73c278994be37fba8a2df9f
Maxwell Anna <amaxwell@lcs-mba3091.lcs.on.ca>
2017-10-18 10:18:44 -0400

    Looked at random noise and data types

--- 50 Blank Canvases.playground/Pages/Canvas 1.xcplaygroundpage/Contents.swift
+++ 50 Blank Canvases.playground/Pages/Canvas 1.xcplaygroundpage/Contents.swift
@@ -22,9 +22,32 @@ let canvas = Canvas(width: 400, height: 300)
  
  Use whitespace and comments as appropriate.
  */
-// Replace this comment with your first comment – what is the goal of the code you're about to write?
+// Illustride random noise generation
+for x in stride(from: 0, to: 200, by: 1){
+    
+    // Decide how tall to draw line
+    let toY = random(from: 0, toButNotIncluding: 101)
+    
+    // Draw a verticle line
+    canvas.drawLine(fromX: x, fromY: 0, toX: x, toY: toY)
+}
 
+// Learn about data types
+var name = "Lakefield" // assignment statment
+name = "Lakefield College School"
 
+// Create a number
+let value = 4 // type inference guesses integer
+let anotherValue : Int = 5
+
+// Create a number that is not an interger
+var decimalValue = 4.5
+decimalValue = 5
+let anotherDecimalValue : Double = 5.5
+
+// What are the maximum and minimum possible values?
+Int.min
+Int.max
 
 /*:
  ## Use source control
